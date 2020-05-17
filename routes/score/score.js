cd7a97bce73e0e12289a95fcc592cc2f2239bd67
goddoro <dongho1222@unist.ac.kr>
2020-05-17 10:47:39 +0900

    third commit

--- routes/score/score.js
+++ routes/score/score.js
@@ -33,41 +33,37 @@ router.post('/graph', function(req, res){
 
     console.log("fuck")
 
-    var query = conn.query(`SELECT subject,score,grade from score`, function(err, result) {
+    var korean =  req.body.korean 
+    var math = req.body.math
+    var english = req.body.english
+    var history = req.body.history
+    var tamgu1 = req.body.tamgu1
+    var tamgu2 = req.body.tamgu2
 
-        console.log(result)
 
-        var korean = ( result.find(it => it.subject == "korean") )
-        var math = ( result.find(it => it.subject == "math"))
-        var english = ( result.find(it => it.subject == "english"))
-        var history =( result.find(it => it.subject == "history"))
-        var tamgu1 = ( result.find(it => it.subject == "tamgu1"))
-        var tamgu2 = ( result.find(it => it.subject == "tamgu2"))
+    var new_korean = parseInt( ( korean.score  * 357.1 ) / 200)
+    var new_math = parseInt ( ( math.score * 357.1) / 200 )
+    var new_tamgu = parseInt(( ( tamgu1.score + tamgu2.score ) * 285.7 ) / 200)
+    var new_english = ( english.grade * 2 - 3 )
+    var new_history = 0
+    if ( history.grade < 4 ) new_history = 10
+    else if ( history.grade > 3 && history.grade < 8 ) {
+        new_history = 10 - 0.2 * ( history.grade - 3)
+    }
+    else new_history = 9
 
-  
-        var new_korean = parseInt( ( korean.score  * 357.1 ) / 200)
-        var new_math = parseInt ( ( math.score * 357.1) / 200 )
-        var new_tamgu = parseInt(( ( tamgu1.score + tamgu2.score ) * 285.7 ) / 200)
-        var new_english = ( english.grade * 2 - 3 )
-        var new_history = 0
-        if ( history.grade < 4 ) new_history = 10
-        else if ( history.grade > 3 && history.grade < 8 ) {
-            new_history = 10 - 0.2 * ( history.grade - 3)
-        }
-        else new_history = 9
+    console.log(new_korean)
+    console.log(new_math)
 
-        console.log(new_korean)
-        console.log(new_math)
+    var total = new_korean + new_math + new_tamgu - new_english + new_history
 
-        var total = new_korean + new_math + new_tamgu - new_english + new_history
+    var object = {
+        "total" : total,
+        "success" : true
+    }
 
-        var object = {
-            "total" : total,
-            "success" : true
-        }
-
-        res.send(object)
-    })
+    res.send(object)
+    
 })
 
 router.post('/list', function(req, res) {
