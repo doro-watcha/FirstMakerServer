2935ede4ca94333d9883abb42ccbff78e4ebb110
goddoro <dongho1222@unist.ac.kr>
2020-05-27 12:05:47 +0900

    [DELETE] dist

--- src/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/services/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var universityService = require('./university');
-
-module.exports = {
-  universityService: universityService
-};
--- src/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/dist/routes/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'gusgh0705',
-  port: '3306',
-  database: 'daehakga'
-};
