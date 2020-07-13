6645dc2907a7c7e493e51c5cbd9e245e802e0a2e
goddoro <dongho1222@unist.ac.kr>
2020-07-13 13:12:02 +0900

    200712 13:12

--- src/routes/academy.js
+++ src/routes/academy.js
@@ -6,6 +6,41 @@ const { authenticate } = Authenticator
 
 const router = new Router()
 
+/**
+ * @swagger
+ *
+ * /academy
+ *   get:
+ *     tags:
+ *       - academy
+ *     summary: 모든 학원 리스트 조회 
+ *     responses:
+ *       SUCCESS:
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     academy:
+ *                       $ref: '#/components/schemas/Academy'
+ *                   required:
+ *                     - academy
+ *               required:
+ *                 - success
+ *                 - data
+ *       'ecode: 201':
+ *         description: 유효하지 않은 토큰
+ *       'ecode: 100':
+ *         description: Request Body Validation 실패
+ *       'ecode: 700':
+ *         description: 서버 에러
+ */
 
 
 router.get('/' , (req,res) => {
