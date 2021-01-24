e0b002082ee247200d52147ac309e34e43d7e8d6
goddoro <dongho1222@unist.ac.kr>
2021-01-24 21:43:18 +0900

    test

--- dist/routes/problem.js
+++ dist/routes/problem.js
@@ -64,7 +64,13 @@ function getToday() {
   var year = date.getFullYear();
   var month = ("0" + (1 + date.getMonth())).slice(-2);
   var day = ("0" + date.getDate()).slice(-2);
-  return year + "-" + month + "-" + day;
+  var hour = date.getHours();
+  if (hour < 10) hour = "0" + hour;
+  var minute = date.getMinutes();
+  if (minute < 10) minute = "0" + minute;
+  var second = date.getSeconds();
+  if (second < 10) second = "0" + second;
+  return year + "-" + month + "-" + day + "_" + hour + ":" + minute + ":" + second;
 }
 
 module.exports = router;
